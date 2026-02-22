78e23bd5c9364a944d81b10ce898111f6be18b4b
AnasPunggawa <anaspunggawa8882@gmail.com>
2026-02-22 14:21:08 +0800

    feat(api-gateway): logging auth and user services

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,11 +1,13 @@
 import { createServer, type Server } from 'node:http';
 
+import { loadEnv } from '@configs/env.config';
 import { logError } from '@libs/logger/error.logger';
 import { logInfo } from '@libs/logger/info.logger';
 import { createApp } from './app';
 
 export function startServer(PORT: number, HOST: string): Server {
   const app = createApp();
+  const { AUTH_SERVICE_URL, USER_SERVICE_URL } = loadEnv();
 
   const server = createServer(app);
 
@@ -17,6 +19,8 @@ export function startServer(PORT: number, HOST: string): Server {
 
   server.listen(PORT, HOST, () => {
     logInfo(`Server is running on http://${HOST}:${PORT}`, 'SERVER');
+    logInfo(`Auth Service is running on ${AUTH_SERVICE_URL}`, 'SERVICE');
+    logInfo(`Users Service is running on ${USER_SERVICE_URL}`, 'SERVICE');
   });
 
   return server;
